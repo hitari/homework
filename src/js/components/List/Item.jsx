a9b0782a127402236e3bd59980448f2a10d371f7
hitari <hitari@naver.com>
2021-06-04 22:43:52 +0900

    마크업 기본 뼈대 구축

---
+++ src/js/components/List/Item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CharacterPage = () => {
+  return (
+    <div>
+      <p>
+        <span></span>
+      </p>
+    </div>
+  );
+};
+export default CharacterPage;
--- src/js/features/CharacterPage.jsx
+++ src/js/features/CharacterPage.jsx
@@ -1,62 +1,78 @@
 import React from 'react';
+import './CharacterPage.css';
 
 const CharacterPage = () => {
   return (
     <div>
-      <div>무신사 과제</div>
+      <div className="header">
+        <h1>무신사 과제</h1>
+      </div>
       <div>
-        <ul>
-          <li>
+        <ul className="filter">
+          <li className="filter_item">
             <input type="checkbox" id="filter0" />
-            <label for="filter0">
+            <label htmlFor="filter0" className="filter_label">
               <span>생존인물만</span>
             </label>
           </li>
-          <li>
+          <li className="filter_item">
             <input type="checkbox" id="filter1" />
-            <label for="filter1">
+            <label htmlFor="filter1" className="filter_label">
               <span>여자</span>
             </label>
           </li>
-          <li>
+          <li className="filter_item">
             <input type="checkbox" id="filter2" />
-            <label for="filter2">
+            <label htmlFor="filter2" className="filter_label">
               <span>tvSeries 없음</span>
             </label>
           </li>
-          <li>
+          <li className="filter_item">
             <input type="checkbox" id="filter3" />
-            <label for="filter3">
+            <label htmlFor="filter3" className="filter_label">
               <span>초기화</span>
             </label>
           </li>
         </ul>
       </div>
       <div>
-        <div>
+        <div className="character_item">
           <dl>
-            <div>
-              <dt>name</dt>
-              <dd>홍길동</dd>
+            <div className="unit_info">
+              <dt className="title">name</dt>
+              <dd className="content">홍길동</dd>
             </div>
-            <div>
-              <dt>aliases</dt>
-              <dd>소설주인공, 도둑, 열혈청년</dd>
+            <div className="unit_info">
+              <dt className="title">aliases</dt>
+              <dd className="content">소설주인공, 도둑, 열혈청년</dd>
             </div>
-            <div>
-              <dt>title</dt>
-              <dd>신홍길동전, 도둑들</dd>
+            <div className="unit_info">
+              <dt className="title">title</dt>
+              <dd className="content">신홍길동전, 도둑들</dd>
             </div>
-            <div>
-              <dt>books</dt>
-              <dd>3</dd>
+            <div className="unit_info">
+              <dt className="title">books</dt>
+              <dd className="content">3</dd>
             </div>
-            <div>
-              <dt>tvSeries</dt>
-              <dd>1</dd>
+            <div className="unit_info">
+              <dt className="title">tvSeries</dt>
+              <dd className="content">1</dd>
             </div>
           </dl>
-          <button>삭제</button>
+          <button className="delete_button">
+            <svg
+              className="delete_ico"
+              xmlns="http://www.w3.org/2000/svg"
+              height="427pt"
+              viewBox="-40 0 427 427.00131"
+              width="427pt"
+            >
+              <path d="m232.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0" />
+              <path d="m114.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0" />
+              <path d="m28.398438 127.121094v246.378906c0 14.5625 5.339843 28.238281 14.667968 38.050781 9.285156 9.839844 22.207032 15.425781 35.730469 15.449219h189.203125c13.527344-.023438 26.449219-5.609375 35.730469-15.449219 9.328125-9.8125 14.667969-23.488281 14.667969-38.050781v-246.378906c18.542968-4.921875 30.558593-22.835938 28.078124-41.863282-2.484374-19.023437-18.691406-33.253906-37.878906-33.257812h-51.199218v-12.5c.058593-10.511719-4.097657-20.605469-11.539063-28.03125-7.441406-7.421875-17.550781-11.5546875-28.0625-11.46875h-88.796875c-10.511719-.0859375-20.621094 4.046875-28.0625 11.46875-7.441406 7.425781-11.597656 17.519531-11.539062 28.03125v12.5h-51.199219c-19.1875.003906-35.394531 14.234375-37.878907 33.257812-2.480468 19.027344 9.535157 36.941407 28.078126 41.863282zm239.601562 279.878906h-189.203125c-17.097656 0-30.398437-14.6875-30.398437-33.5v-245.5h250v245.5c0 18.8125-13.300782 33.5-30.398438 33.5zm-158.601562-367.5c-.066407-5.207031 1.980468-10.21875 5.675781-13.894531 3.691406-3.675781 8.714843-5.695313 13.925781-5.605469h88.796875c5.210937-.089844 10.234375 1.929688 13.925781 5.605469 3.695313 3.671875 5.742188 8.6875 5.675782 13.894531v12.5h-128zm-71.199219 32.5h270.398437c9.941406 0 18 8.058594 18 18s-8.058594 18-18 18h-270.398437c-9.941407 0-18-8.058594-18-18s8.058593-18 18-18zm0 0" />
+              <path d="m173.398438 154.703125c-5.523438 0-10 4.476563-10 10v189c0 5.519531 4.476562 10 10 10 5.523437 0 10-4.480469 10-10v-189c0-5.523437-4.476563-10-10-10zm0 0" />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
